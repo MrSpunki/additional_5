b62ec977e0ece0183e9733abd5ce9644e962ce73
MrSpunki <36659232+MrSpunki@users.noreply.github.com>
2018-09-30 10:59:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  var arrayElements = str.split("");
+  
+  for (i = 0; i <= arrayElements.length; i++) {
+    for (j = 0; j < arrayElements.length; j++) {
+      
+      if (((arrayElements[j] === '(') && (arrayElements[j+1] === ')'))
+        ||((arrayElements[j] === '[') && (arrayElements[j+1] === ']'))
+        ||((arrayElements[j] === '{') && (arrayElements[j+1] === '}'))
+        ||((arrayElements[j] === '|') && (arrayElements[j+1] === '|'))) {
+          
+          arrayElements.splice(j, 2);
+      }
+    }
+  }
+
+  if (arrayElements.length === 0) {
+    return true;
+  }else {
+    return false;
+  }
 }
